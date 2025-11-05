2de83e5ed6fac4bd2bcc773ff0fccb2a3525a3d1
luisito666 <luispenagos91@gmail.com>
2025-11-05 00:00:28 -0500

    Fix problem with markdown on some sites of the web

--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import About, Skill, Project
+import markdown
 
 class HomeView(TemplateView):
     """Main portfolio homepage view"""
@@ -10,7 +11,11 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         
         # Get about section (most recent one)
-        context['about'] = About.objects.first()
+        about = About.objects.first()
+        if about:
+            # Convert markdown content to HTML for about section
+            context['about_html'] = markdown.markdown(about.content, extensions=['extra', 'codehilite'])
+        context['about'] = about
         
         # Get skills grouped by category
         skills_by_category = {}
@@ -21,8 +26,12 @@ class HomeView(TemplateView):
             skills_by_category[skill.category].append(skill)
         context['skills_by_category'] = skills_by_category
         
-        # Get projects (featured first)
-        context['projects'] = Project.objects.all()
+        # Get projects (featured first) and process markdown
+        projects = Project.objects.all()
+        for project in projects:
+            # Convert markdown description to HTML for project cards
+            project.description_html = markdown.markdown(project.description, extensions=['extra', 'codehilite'])
+        context['projects'] = projects
         
         return context
 
@@ -32,5 +41,8 @@ class ProjectDetailView(TemplateView):
     
     def get_context_data(self, pk, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['project'] = Project.objects.get(pk=pk)
+        project = Project.objects.get(pk=pk)
+        # Convert markdown description to HTML
+        context['project_description_html'] = markdown.markdown(project.description, extensions=['extra', 'codehilite'])
+        context['project'] = project
         return context
