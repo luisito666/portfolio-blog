286b03aecc08d4eb1d8ddd6c2f20f5a07fad6c11
luisito666 <luispenagos91@gmail.com>
2025-12-09 22:32:19 -0500

    Add value for manage csfr

--- src/core/settings.py
+++ src/core/settings.py
@@ -29,6 +29,11 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS', 
+    default='localhost,127.0.0.1',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 # Application definition
 
